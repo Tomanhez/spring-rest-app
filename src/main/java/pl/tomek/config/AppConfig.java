197f02cf648ae4483cd7eb2df7b2f10771760d50
TomaszSut <automek123@gmail.com>
2019-01-03 21:51:27 +0100

    init app

---
+++ src/main/java/pl/tomek/config/AppConfig.java
@@ -0,0 +1,13 @@
+package pl.tomek.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("pl.tomek")
+public class AppConfig {
+
+	
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<hr>
+<h2>Spring Rest Demo !</h2>
+<hr>
+<a href="test/hello">Hello</a>
+</body>
+</html>
